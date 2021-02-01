6545f82ffd719e3afc04d685689627201d27a56d
jela3105 <jela3105@gmail.com>
2021-01-31 20:59:10 -0600

    Conect to Mongo database

--- api/index.js
+++ api/index.js
@@ -2,3 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://root:n0m3l0@cluster0.4p7ij.mongodb.net/Cluster0?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+app.get("*", (req, res) => {
+  res.send("Hola Mundo");
+});
+module.exports = app;
