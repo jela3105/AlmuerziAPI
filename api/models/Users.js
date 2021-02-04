ad5e402fe674c9537c000d58526eb327ed2b1b38
jela3105 <jela3105@gmail.com>
2021-02-03 19:29:10 -0600

    Create user model

---
+++ api/models/Users.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Users = mongoose.model(
+  "Users",
+  new Schema({ email: String, password: String, salt: String })
+);
+
+module.exports = Users;
